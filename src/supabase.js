48067479c4563420e87e8145178dc8a432d979f2
ultravsiolence <chinmayee.latkar@gmail.com>
2023-10-15 13:11:44 -0400

    added and connected supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseURL = "https://wtixgxipwrvwgwknuizn.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0aXhneGlwd3J2d2d3a251aXpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTczODY1MzksImV4cCI6MjAxMjk2MjUzOX0.o2C-VMW8adimeAMck3NFlYcsTXNLqs43kxOWJcOfpdI";
+const supabaseURL = process.env.REACT_APP_supabaseURL;
+const supabaseKey = process.env.REACT_APP_supabaseKey;
 
 export const supabase = createClient(supabaseURL, supabaseKey)
