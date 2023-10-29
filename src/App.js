e92bead71a1b651b4615e840e77cc5edb9ee2f20
Nalnoc <conlanfoulkes2@gmail.com>
2023-10-29 15:13:20 -0400

    implement spotify album search

--- src/App.js
+++ src/App.js
@@ -35,26 +35,34 @@ export default function App() {
   const [genre, setGenre] = useState("");
   const [releaseDate, setReleaseDate] = useState("");
 
+  const [albums, setAlbums] = useState([]);
   const[searchInput, setSearchInput] = useState("");
   async function search() {
     console.log("Search for " + searchInput);
 
     //Get request using search to get Artist ID
-    var artistParameters = {
+    var searchParameters = {
       method: 'GET',
       headers: {
         'Content-Type' : 'application/json',
         'Authorization'  :'Bearer ' + spotifyToken
       }
     }
-    var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', artistParameters)
+    var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', searchParameters)
       .then(response => response.json())
-      .then(data => console.log(data))
-    //Get rquest with Artist ID grab all albums
+      .then(data => {return data.artists.items[0].id})
 
+    console.log('Artist ID is ' + artistID)
+    //Get request with Artist ID grab all albums
+    var returnedAlbums = await fetch('https://api.spotify.com/v1/artists/'+artistID+'/albums' + '?include_groups=album&market=US&limit=50', searchParameters)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setAlbums(data.items);
+      })
     //Display albums
   }
-  
+  console.log(albums);
 
   //spotify login
   const [spotifyToken, setSpotifyToken] = useState("");
@@ -138,13 +146,18 @@ export default function App() {
         </InputGroup>
       </Container>
       <Container>
-        <Row className='mx-2 row row-cols-5'>
-        <Card>
-          <Card.Img src="#" />
-          <Card.Body>
-            <Card.Title>Album Name Here</Card.Title>
-          </Card.Body>
-        </Card>
+        <Row className='no-gutters row row-cols-3'>
+          {albums.map( (album, i) => {
+            console.log(album);
+            return(
+              <Card className = 'text-white bg-info'>
+                <Card.Img src={album.images[0].url} />
+                <Card.Body>
+                  <Card.Title>{album.name}</Card.Title>
+                </Card.Body>
+              </Card>
+            )
+          })}
         </Row>
       </Container>
 
